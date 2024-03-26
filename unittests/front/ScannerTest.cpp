9e8674da8ea906aaaba7e020ddd042d3cdaf0f1f
LucasEBSkora <skora.lucas@gmail.com>
2024-03-26 07:06:44 -0300

    fixes cmake so it actually rebuilds scanner and parser files

--- unittests/front/ScannerTest.cpp
+++ unittests/front/ScannerTest.cpp
@@ -1,6 +1,5 @@
 #include "gtest/gtest.h"
 #include "scanner.h"
-
 #include <iostream>
 
 namespace Gnocchi
@@ -27,11 +26,26 @@ namespace Gnocchi
     {
       ASSERT_NE(nullptr, obj);
     }
+
+    void testSwitchStream() {
+      std::stringstream stream;
+      stream << "";
+      obj->switch_streams(&stream, nullptr);
+      ASSERT_EQ(0, obj->get_next_token().kind());
+
+      stream << "+";
+      ASSERT_NE(0, obj->get_next_token().kind());
+    }
   };
 
   TEST_F(ScannerTest, testCreateScanner)
   {
     this->testCreateScanner();
+  }
+
+  TEST_F(ScannerTest, testSwitchStream)
+  {
+    this->testSwitchStream();
   }
 
 }
